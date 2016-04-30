aabb56494246fbc532ce986652e053288a656ae5
Igor Poplavsky <Igor Poplavsky>
2016-04-30 22:17:58 +0300

    refactoring

--- server/index.js
+++ server/index.js
@@ -1,48 +1,72 @@
 import fs from 'fs';
 import path from 'path';
-import inquirer from 'inquirer';
+import pug from 'pug';
 import express from 'express';
 import serveStatic from 'serve-static';
 
 const ROOT_PATH = path.resolve(__dirname, '..');
 const SRC_PATH = path.resolve(ROOT_PATH, 'src');
+const TEMPLATES_PATH = path.resolve(SRC_PATH, 'templates');
 const TASKS_PATH = path.resolve(ROOT_PATH, 'tasks');
+const MODULES_PATH = path.resolve(ROOT_PATH, 'node_modules');
 
 const fileNames = fs.readdirSync(TASKS_PATH);
-const testNames = fileNames.filter(fileName => fs.statSync(path.resolve(TASKS_PATH, fileName)).isDirectory());
+const taskNames = fileNames.filter(fileName => fs.statSync(path.resolve(TASKS_PATH, fileName)).isDirectory());
 
+const indexTemplate = path.resolve(TEMPLATES_PATH, 'index.pug');
+const pureTemplate = path.resolve(TEMPLATES_PATH, 'pure.pug');
+const testTemplate = path.resolve(TEMPLATES_PATH, 'test.pug');
 
-inquirer.prompt([
-    {
-      type: 'list',
-      name: 'entry',
-      choices: testNames,
-      message: 'Which entry do you want to serve?'
-    }
-]).then(answers => init(answers));
+const app = express();
+const port = 3000;
 
-function init({ entry }) {
-  const app = express();
-  const port = 3000;
-  const taskPath = path.resolve(TASKS_PATH, entry);
-  const indexPath = path.resolve(SRC_PATH, 'index.html');
+app.get('/', (req, res) => {
+  return res.send(
+    pug.renderFile(indexTemplate, {
+      pageTitle: 'Tasks',
+      taskNames: taskNames
+    })
+  );
+});
 
-  app.get('/', (req, res) => {
-    res.send(
-      fs.readFileSync(
-        path.resolve(indexPath),
-        { encoding: 'utf-8' }
-      ).replace('{{title}}', entry)
-    );
-  });
-  app.use('/', serveStatic(SRC_PATH));
-  app.use('/js', serveStatic(taskPath));
+app.use('/assets', serveStatic(SRC_PATH));
+app.use('/assets/vendor', serveStatic(MODULES_PATH));
+app.use('/tasks', serveStatic(TASKS_PATH));
 
-  app.listen(port, err => {
-    if (err) {
-      throw err;
-    }
+app.get('/test/:taskName', (req, res) => {
+  const { taskName } = req.params;
 
-    console.log('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
-  });
-}
+  if (taskNames.indexOf(taskName) === -1) {
+    return res.status(404).send('404 Not Found');
+  }
+
+  return res.send(
+    pug.renderFile(testTemplate, {
+      pageTitle: taskName,
+      taskName: taskName
+    })
+  );
+});
+
+app.get('/pure/:taskName', (req, res) => {
+  const { taskName } = req.params;
+
+  if (taskNames.indexOf(taskName) === -1) {
+    return res.status(404).send('404 Not Found');
+  }
+
+  return res.send(
+    pug.renderFile(pureTemplate, {
+      pageTitle: taskName,
+      taskName: taskName
+    })
+  );
+});
+
+app.listen(port, err => {
+  if (err) {
+    throw err;
+  }
+
+  console.log('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
+});
